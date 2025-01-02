5ccedc7de147caf3fe6f4b25c4c469b701410830
Haniya-Shuja <haniyashuja786@gmail.com>
2025-01-02 11:20:10 -0800

    success

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,43 +1,54 @@
+// import AdminContact from '@/components/adminContact/adminContact';
+// import AdminPostForm from '@/components/adminPostForm/adminPostForm';
+// import AdminPosts from '@/components/adminPosts/adminPosts';
+// import AdminUserForm from '@/components/adminUserForm/adminUserForm';
+// import AdminUsers from '@/components/adminUsers/adminUsers';
+// // import { getSession } from 'next-auth/react';
+// import { Suspense } from 'react';
 
-import React, { Suspense } from 'react'
-import styles from "./admin.module.css";
-import AdminPosts from '@/components/adminPosts/adminPosts';
-import AdminPostForm from '@/components/adminPostForm/adminPostForm';
-import AdminUsers from '@/components/adminUsers/adminUsers';
-import AdminUserForm from '@/components/adminUserForm/adminUserForm';
-import AdminContact from '@/components/adminContact/adminContact';
+// const AdminPage = async () => {
+//     // const session = await getSession(); // Fetch the session
 
-const AdminPage = async () => {
-    const session = await auth();
-     
-  return (
-    <div className={styles.container}>
-      <div className={styles.row}>
-        <div className={styles.col}>
-            <Suspense fallback={<div>Loading...</div>}>
-                <AdminPosts />
-            </Suspense>
-        </div>
-        <div className={styles.col}>
-            <AdminPostForm userId={session.user.id} />
-        </div>
-      </div>
-      <div className={styles.row}>
-        <div className={styles.col}>
-            <Suspense fallback={<div>Loading...</div>}>
-                <AdminUsers />
-            </Suspense>
-        </div>
-        <div className={styles.col}>
-            <AdminUserForm />
-        </div>
-      </div>
-      <div>
-        <h1 className="text-center text-xl">Contact information</h1>
-        <AdminContact />
-      </div>
-    </div>
+//     // if (!session) {
+//     //     return <p>You need to be logged in to access this page.</p>;
+//     // }
+
+//     return (
+//         <div className={styles.container}>
+//             <div className={styles.row}>
+//                 <div className={styles.col}>
+//                     <Suspense fallback={<div>Loading...</div>}>
+//                         <AdminPosts />
+//                     </Suspense>
+//                 </div>
+//                 <div className={styles.col}>
+//                     <AdminPostForm userId={1} />
+//                 </div>
+//             </div>
+//             <div className={styles.row}>
+//                 <div className={styles.col}>
+//                     <Suspense fallback={<div>Loading...</div>}>
+//                         <AdminUsers />
+//                     </Suspense>
+//                 </div>
+//                 <div className={styles.col}>
+//                     <AdminUserForm />
+//                 </div>
+//             </div>
+//             <div>
+//                 <h1 className="text-center text-xl">Contact information</h1>
+//                 <AdminContact />
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default AdminPage;
+
+
+export default function AdminPage(){
+  return(
+    <>
+    </>
   )
 }
-
-export default AdminPage
